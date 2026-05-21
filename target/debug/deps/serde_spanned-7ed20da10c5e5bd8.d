3dcf24eb69d7f5bb0fe27f24d106b9e751d5206e
Lee Daghlar Ostadi <ostadi.lee@gmail.com>
2026-05-21 20:11:20 +0000

    Start: script import/run feature plan

--- target/debug/deps/serde_spanned-7ed20da10c5e5bd8.d
+++ target/debug/deps/serde_spanned-7ed20da10c5e5bd8.d
@@ -1,8 +1,8 @@
-/home/runner/work/O-lang_rust_edition/O-lang_rust_edition/target/debug/deps/serde_spanned-7ed20da10c5e5bd8.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
+/tmp/workspace/lostadi/O-lang_rust_edition/target/debug/deps/serde_spanned-7ed20da10c5e5bd8.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
 
-/home/runner/work/O-lang_rust_edition/O-lang_rust_edition/target/debug/deps/libserde_spanned-7ed20da10c5e5bd8.rlib: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
+/tmp/workspace/lostadi/O-lang_rust_edition/target/debug/deps/libserde_spanned-7ed20da10c5e5bd8.rlib: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
 
-/home/runner/work/O-lang_rust_edition/O-lang_rust_edition/target/debug/deps/libserde_spanned-7ed20da10c5e5bd8.rmeta: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
+/tmp/workspace/lostadi/O-lang_rust_edition/target/debug/deps/libserde_spanned-7ed20da10c5e5bd8.rmeta: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
 
 /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs:
 /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs:
