1a6550d16d5c32cfbf379326b7d468e1aeb9ba1e
Lee Daghlar Ostadi <ostadi.lee@gmail.com>
2026-06-02 20:42:24 +0000

    chore: plan repository reorganization

--- target/debug/deps/olangc-b65616aaa4231af8.d
+++ target/debug/deps/olangc-b65616aaa4231af8.d
@@ -1,6 +1,6 @@
-/tmp/workspace/lostadi/O-lang_rust_edition/target/debug/deps/olangc-b65616aaa4231af8.d: src/bin/olangc.rs src/bin/../value.rs src/bin/../parser.rs src/bin/../eval.rs src/bin/../process.rs src/bin/../nix_ops.rs src/bin/../nixos_ops.rs src/bin/../scheduler.rs src/bin/../../Cargo.lock src/bin/../../backends/nix_shim.py src/bin/../../backends/nix_store_shim.py src/bin/../../backends/nixos_test_shim.py src/bin/../../backends/python_shim.py
+/tmp/workspace/lostadi/O-lang/target/debug/deps/olangc-b65616aaa4231af8.d: src/bin/olangc.rs src/bin/../value.rs src/bin/../parser.rs src/bin/../eval.rs src/bin/../process.rs src/bin/../nix_ops.rs src/bin/../nixos_ops.rs src/bin/../scheduler.rs src/bin/../../Cargo.lock src/bin/../../backends/nix_shim.py src/bin/../../backends/nix_store_shim.py src/bin/../../backends/nixos_test_shim.py src/bin/../../backends/python_shim.py
 
-/tmp/workspace/lostadi/O-lang_rust_edition/target/debug/deps/olangc-b65616aaa4231af8: src/bin/olangc.rs src/bin/../value.rs src/bin/../parser.rs src/bin/../eval.rs src/bin/../process.rs src/bin/../nix_ops.rs src/bin/../nixos_ops.rs src/bin/../scheduler.rs src/bin/../../Cargo.lock src/bin/../../backends/nix_shim.py src/bin/../../backends/nix_store_shim.py src/bin/../../backends/nixos_test_shim.py src/bin/../../backends/python_shim.py
+/tmp/workspace/lostadi/O-lang/target/debug/deps/olangc-b65616aaa4231af8: src/bin/olangc.rs src/bin/../value.rs src/bin/../parser.rs src/bin/../eval.rs src/bin/../process.rs src/bin/../nix_ops.rs src/bin/../nixos_ops.rs src/bin/../scheduler.rs src/bin/../../Cargo.lock src/bin/../../backends/nix_shim.py src/bin/../../backends/nix_store_shim.py src/bin/../../backends/nixos_test_shim.py src/bin/../../backends/python_shim.py
 
 src/bin/olangc.rs:
 src/bin/../value.rs:
